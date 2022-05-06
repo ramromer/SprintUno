8f493f3401026307bf073a0ed8b7db7b6636a960
Rama <ramrromer@Rama-MacBook-Air.local>
2022-05-06 14:13:47 -0300

    HTML: dinamizada la carga de producto [  Modif. /views/index.ejs & /controllers/mainController.js]. Agregadas imagenes random para muestra

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,113 @@
 const path = require('path');
 
+const listaBicis = [
+    {
+        id: 1,
+        titulo: 'Orbea Rise',
+        descripcionCorta: 'Bicicleta',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '10.000',
+        img: 'bicicleta-orbea-rise-m20.jpg',
+        alt: 'bici',
+    },
+    {
+        id: 2,
+        titulo: 'Orbea Rise M21',
+        descripcionCorta: 'Bicicleta',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '11.000',
+        img: 'bicicleta-orbea-rise-m21.jpg',
+        alt: 'bici',
+    },
+    {
+        id: 3,
+        titulo: 'Aurorita Plegable',
+        descripcionCorta: 'Bicicleta plegable',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '12.000',
+        descuento: '10%',
+        img: 'bicicleta-aurorita-cyan.png',
+        alt: 'bici',
+    },
+    {
+        id: 4,
+        titulo: 'Aurorita Plegable',
+        descripcionCorta: 'Bicicleta plegable',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '12.000',
+        img: 'bicicleta-aurorita-green.png',
+        alt: 'bici',
+    },
+    {
+        id: 5,
+        titulo: 'Aurorita Plegable',
+        descripcionCorta: 'Bicicleta plegable',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '12.000',
+        img: 'bicicleta-aurorita-orange.png',
+        alt: 'bici',
+    },
+    {
+        id: 6,
+        titulo: 'Aurorita Plegable',
+        descripcionCorta: 'Bicicleta plegable',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '12.000',
+        img: 'bicicleta-aurorita-paleyellow.png',
+        alt: 'bici',
+    },
+    {
+        id: 7,
+        titulo: 'Topmega',
+        descripcionCorta: 'Bicicleta MTB',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '13.000',
+        descuento: '10%',
+        img: 'bicicleta-topmega-cyan.png',
+        alt: 'bici',
+    },
+    {
+        id: 8,
+        titulo: 'Topmega',
+        descripcionCorta: 'Bicicleta MTB',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '13.000',
+        img: 'bicicleta-topmega-yellow.png',
+        alt: 'bici',
+    },
+    {
+        id: 9,
+        titulo: 'Topmega',
+        descripcionCorta: 'Bicicleta MTB',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '13.000',
+        img: 'bicicleta-topmega-red.png',
+        alt: 'bici',
+    },
+    {
+        id: 10,
+        titulo: 'SLP',
+        descripcionCorta: 'Bicicleta plegable SLP',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '9.000',
+        img: 'bicicleta-slp-yellow.png',
+        alt: 'bici',
+    },
+    {
+        id: 11,
+        titulo: 'SLP',
+        descripcionCorta: 'Bicicleta plegable SLP',
+        descripcionDetallada: 'Descripcion Ampliada',
+        precio: '9.000',
+        img: 'bicicleta-slp-white.png',
+        alt: 'bici',
+    }
+]
+
+
 let mainController = {
-    index: (req,res) => {
-        res.render('index.ejs')
+    index: (req, res) => {
+        res.render('index.ejs',{listaBicis:listaBicis})
     },
 
     detalleProducto: (req, res) => {
