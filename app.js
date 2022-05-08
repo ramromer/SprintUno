1e0a1e31340494beb7dd34076c2b10728918cab7
luis.iglesias <luisjavi27@gmail.com>
2022-05-08 16:54:03 -0500

    se arregla error debido a a que se eliminaron archivs independientes de vista carrito

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const carro = require('./routes/carrito');
 
 var app = express();
 
@@ -22,7 +21,6 @@ app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/carrito',carro);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
