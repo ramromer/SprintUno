dc285885fbc395c9dfce9b552f5ccddd652ac3df
luis.iglesias <luisjavi27@gmail.com>
2022-05-07 22:16:52 -0500

    add vista prodcuto nuevo, falta detalles, se modifico el slider para que permita ver varias imagenes en detalle producto, se modifico el objeto que almacena la info de las bicis

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var mainController = require('../controllers/mainController.js');
 /* GET home page. */
 router.get('/', mainController.index);
 
-router.get('/detalleproducto', mainController.detalleProducto);
+router.get('/detalleproducto/:id', mainController.detalleProducto);
+
+router.get('/productoNuevo', mainController.productoNuevo);
 
 module.exports = router;
