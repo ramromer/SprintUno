6ebfe33c9f8fc09b0be4dd97e92557482b0339bc
luis.iglesias <luisjavi27@gmail.com>
2022-05-10 16:23:30 -0500

    maás cambios menores de estilo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var mainController = require('../controllers/mainController.js');
+let express = require('express');
+let router = express.Router();
+let mainController = require('../controllers/mainController.js');
 
 /* GET home page. */
 router.get('/', mainController.index);
