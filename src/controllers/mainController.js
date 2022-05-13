d394afcfa16a5bf4274a86ff61a80d8fc1fed6ef
luis.iglesias <luisjavi27@gmail.com>
2022-05-12 21:56:32 -0500

    fix error al redireccionar luego de crear nuevo producto

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -23,6 +23,44 @@ let mainController = {
     productoNuevo: (req, res) => {
         res.render('./products/productoNuevo')
     },
+    crearproductoNuevo: (req, res) => {
+          //esto va aca??
+    let listaBicisFile = fs.readFileSync(path.join(__dirname,'../data/data.json'));
+    let listaBicis = JSON.parse(listaBicisFile);
+    let ultimoElmnt = listaBicis[listaBicis.length-1];
+    let productoNuevo={
+    id: ultimoElmnt.id+1,
+    titulo: req.body.nombre,
+    descripcionCorta: req.body.descripcionProductoNuevo,
+    descripcionDetallada: "Descripcion Ampliada", //req.body.descripcionAmpliada,
+    colorDisponible: [ //req.body.color
+        "white",
+        "red",
+        "black"
+    ],
+    tamañoDisponible: [ //req.body.tamanio
+        "S",
+        "M",
+        "L"
+    ],
+    cantidadDisponible: req.body.cantidad,
+    precio: req.body.precio,
+    category: "visited", //req.body.category
+    img: [
+        req.file.filename
+    ],
+    alt: "bici"
+    }
+    
+    listaBicis.push(productoNuevo);
+    console.log(listaBicis);
+
+    let salida = JSON.stringify(listaBicis, null, " ");
+    fs.writeFile(path.join(__dirname,'../data/data.json'), salida, ()=>{}) ;
+
+    // res.redirect(`./detalleproducto/${productoNuevo.id}`);
+    res.redirect("/");
+    },
 
     editarProducto: (req, res) => {
         let reqId = listaBicis.find(element => element.id == req.params.id);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,7 @@ let router = express.Router();
 let mainController = require('../controllers/mainController.js');
 let multer = require('multer');
 let path = require('path');
-const req = require('express/lib/request');
-const fs = require('fs');
+
 
 
 
@@ -28,44 +27,7 @@ router.get('/productonuevo', mainController.productoNuevo);
 router.get('/editarproducto/:id', mainController.editarProducto);
 router.get('error', mainController.error);
 
-router.post('/productonuevo', uploadFile.single('image'), (req,res)=>
-{
-    //esto va aca??
-    let listaBicisFile = fs.readFileSync(path.join(__dirname,'../data/data.json'));
-    let listaBicis = JSON.parse(listaBicisFile);
-    let ultimoElmnt = listaBicis[listaBicis.length-1];
-    let productoNuevo={
-    id: ultimoElmnt.id+1,
-    titulo: req.body.nombre,
-    descripcionCorta: req.body.descripcionProductoNuevo,
-    descripcionDetallada: "Descripcion Ampliada", //req.body.descripcionAmpliada,
-    colorDisponible: [ //req.body.color
-        "white",
-        "red",
-        "black"
-    ],
-    tamañoDisponible: [ //req.body.tamanio
-        "S",
-        "M",
-        "L"
-    ],
-    cantidadDisponible: req.body.cantidad,
-    precio: req.body.precio,
-    category: "visited", //req.body.category
-    img: [
-        req.file.filename
-    ],
-    alt: "bici"
-    }
-    
-    listaBicis.push(productoNuevo);
-    console.log(listaBicis);
-
-    let salida = JSON.stringify(listaBicis);
-    fs.writeFileSync(path.join(__dirname,'../data/data.json'), salida);
-
-    res.redirect('/');
-});
+router.post('/productonuevo', uploadFile.single('image'), mainController.crearproductoNuevo);
 
 // agregar nuevo elemento al array (push) y luego hacer JSON.stringify()
 // fileWrite. res.redirect....
