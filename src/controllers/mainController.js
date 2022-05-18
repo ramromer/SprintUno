09d05ca83167c02a908ba2d84b3492962f72b9b3
luis.iglesias <luisjavi27@gmail.com>
2022-05-17 21:18:48 -0500

    fix ruta crear

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,75 +1,86 @@
-
-const fs=require('fs');
-const path=require('path');
-
-
-let listaBicisFile = fs.readFileSync(path.join(__dirname,'../data/data.json'));
-let listaBicis = JSON.parse(listaBicisFile);
+const fs = require("fs");
+const path = require("path");
 
 let mainController = {
-    index: (req, res) => {
-        res.render('index.ejs',{listaBicis:listaBicis})
-    },
+  index: (req, res) => {
+    let listaBicis = getListaBicis();   
+    res.render("index.ejs", { listaBicis: listaBicis });
+  },
 
-    carrito: (req,res) => {
-        res.render('./products/carrito');
-    },
+  carrito: (req, res) => {
+    res.render("./products/carrito");
+  },
 
-    detalleProducto: (req, res) => {
-        let reqId = listaBicis.find(element => element.id == req.params.id);
-        res.render('./products/detalleProducto', {producto:reqId})
-    },
+  detalleProducto: (req, res) => {
+    let listaBicis = getListaBicis();
+    let reqId = listaBicis.find((element) => element.id == req.params.id);
+    res.render("./products/detalleProducto", { producto: reqId });
+  },
 
-    productoNuevo: (req, res) => {
-        res.render('./products/productoNuevo')
-    },
-    crearproductoNuevo: (req, res) => {
-          //esto va aca??
-    let listaBicisFile = fs.readFileSync(path.join(__dirname,'../data/data.json'));
-    let listaBicis = JSON.parse(listaBicisFile);
-    let ultimoElmnt = listaBicis[listaBicis.length-1];
-    let productoNuevo={
-    id: ultimoElmnt.id+1,
-    titulo: req.body.nombre,
-    descripcionCorta: req.body.descripcionProductoNuevo,
-    descripcionDetallada: "Descripcion Ampliada", //req.body.descripcionAmpliada,
-    colorDisponible: [ //req.body.color
-        "white",
-        "red",
-        "black"
-    ],
-    tamañoDisponible: [ //req.body.tamanio
-        "S",
-        "M",
-        "L"
-    ],
-    cantidadDisponible: req.body.cantidad,
-    precio: req.body.precio,
-    category: "visited", //req.body.category
-    img: [
-        req.file.filename
-    ],
-    alt: "bici"
+  productoNuevo: (req, res) => {
+    res.render("./products/productoNuevo");
+  },
+  crearproductoNuevo: (req, res) => {
+    if (req.file) {
+      let listaBicisFile = fs.readFileSync(
+        path.join(__dirname, "../data/data.json")
+      );
+      let listaBicis = JSON.parse(listaBicisFile);
+      let ultimoElmnt = listaBicis[listaBicis.length - 1];
+      let productoNuevo = {
+        id: ultimoElmnt.id + 1,
+        titulo: req.body.nombre,
+        descripcionCorta: req.body.descripcionProductoNuevo,
+        descripcionDetallada: "Descripcion Ampliada", //req.body.descripcionAmpliada,
+        colorDisponible: [
+          //req.body.color
+          "white",
+          "red",
+          "black",
+        ],
+        tamañoDisponible: [
+          //req.body.tamanio
+          "S",
+          "M",
+          "L",
+        ],
+        cantidadDisponible: req.body.cantidad,
+        precio: req.body.precio,
+        category: "visited", //req.body.category
+        img: [req.file.filename],
+        alt: "bici",
+      };
+
+      listaBicis.push(productoNuevo);
+
+      let salida = JSON.stringify(listaBicis, null, " ");
+      let fullPath = path.join(__dirname, "../data/data.json");
+
+      fs.writeFileSync(fullPath, salida);
+
+      res.redirect(`./detalleproducto/${productoNuevo.id}`);
+
+    } else {
+      res.redirect("/productonuevo");
     }
-    
-    listaBicis.push(productoNuevo);
-    console.log(listaBicis);
+  },
 
-    let salida = JSON.stringify(listaBicis, null, " ");
-    fs.writeFile(path.join(__dirname,'../data/data.json'), salida, ()=>{}) ;
+  editarProducto: (req, res) => {
+    let reqId = listaBicis.find((element) => element.id == req.params.id);
+    res.render("./products/editarProducto", { producto: reqId });
+  },
 
-    // res.redirect(`./detalleproducto/${productoNuevo.id}`);
-    res.redirect("/");
-    },
+  error: (req, res) => {
+    res.render("error");
+  },
+};
 
-    editarProducto: (req, res) => {
-        let reqId = listaBicis.find(element => element.id == req.params.id);
-        res.render('./products/editarProducto', {producto:reqId})
-    },
-
-    error: (req, res) => {
-        res.render('error')
-    },
+function getListaBicis() {
+  let listaBicisFile = fs.readFileSync(
+    path.join(__dirname, "../data/data.json")
+  );
+  let listaBicis = JSON.parse(listaBicisFile);
+  return listaBicis;
 }
 
 module.exports = mainController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,6 @@ let mainController = require('../controllers/mainController.js');
 let multer = require('multer');
 let path = require('path');
 
-
-
-
 const storage = multer.diskStorage({
     destination:  function(req, file, cb) {
         cb(null,'./public/images');
@@ -17,9 +14,6 @@ const storage = multer.diskStorage({
 })
 const uploadFile=multer({storage:storage});
 
-//`` te odio comillas raras
-
-/* GET home page. */
 router.get('/', mainController.index);
 router.get('/carrito', mainController.carrito);
 router.get('/detalleproducto/:id', mainController.detalleProducto);
@@ -29,14 +23,4 @@ router.get('error', mainController.error);
 
 router.post('/productonuevo', uploadFile.single('image'), mainController.crearproductoNuevo);
 
-// agregar nuevo elemento al array (push) y luego hacer JSON.stringify()
-// fileWrite. res.redirect....
-
-
 module.exports = router;
-
-
-// function guardarFotoProducto () {
-//     let archivo = req.body.image;
-
-// };
