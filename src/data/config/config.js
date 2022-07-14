8535248412360afe79d06d92f96beb1a9e72f407
Rama <ramrromer@Rama-MacBook-Air.local>
2022-07-14 20:08:36 -0300

    cambios en detalleProducto.ejs. Ahora se muestran los radio buttons solo de las opciones que efectivamente HAY en stock.

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -10,11 +10,7 @@ module.exports={
   },
   "development": {
     "username": "root",
-<<<<<<< HEAD
-    "password": "",
-=======
     "password": "r00tr00t",
->>>>>>> 532bd96a07757c75c3f6338a0c77b4a9cb412ef1
     "database": "Kletadb",
     "host": "127.0.0.1",
     "dialect": "mysql"
