16e7170c689d6a2e71cc4cdbffe931a07e0490da
Rama <ramrromer@Rama-MacBook-Air.local>
2022-08-02 21:31:00 -0300

    cambios

--- public/js/register.js
+++ public/js/register.js
@@ -1,31 +1,28 @@
-const db = require('../../src/data/models');
 
 
 window.addEventListener('load', function(){
-    
+    if(document.URL.startsWith('http://localhost')){
+        url = 'http://localhost:3000';
+    } else{
+        url = 'https://#MODIFICARESTO.herokuapp.com'; // CAMBIAR AQUI
+    }
+
     let email = document.getElementById("email");
     let yaexiste = document.getElementById("yaexisteemail");
-    
-    email.addEventListener("focusout", function () {
-        esto(email.value);
-    });
-    // console.log('alguito');
-    // Window.alert('hola');
-});
+    let button = document.getElementById("submitBtn");
+    function askRegister(checking) {
+        fetch(`${url}/users/register/${checking}`)//ojo aca luego deberemos cambiar la ruta si hacemos deploy en heroku
+        fetch(`/users/register/${checking}`)//ojo aca luego deberemos cambiar la ruta si hacemos deploy en heroku
+        .then((res)=>{return res.json()})
+        .then((respuesta) => {
+            // console.log(respuesta);
+            respuesta==1? yaexiste.style.display = 'block':yaexiste.style.display = 'none';
+            respuesta==1? button.disabled = true : button.disabled = false;
+        })
+        
+    }
 
-esto = function (asd){
-    return new Promise((resolve, reject) => {
-        db.User.findOne({where:{email:asd}}).then(function(user){
-          
-          if(Boolean(user)) {
-           // reject()
-            console.log('aqui');
-            yaexiste.style.display = 'block'
-          }
-          resolve(true)
-        //   document.getElementById("yaexisteemail").style.setProperty("display","none");
-            yaexiste.style.display = 'none'
-            console.log('aqui')
-        }).catch(err => {console.log(err); reject(new Error('Error en el servidor'))});
-      });
-}
+    email.addEventListener("focusout", function () {
+        askRegister(email.value);
+    });
+});
