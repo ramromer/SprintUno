795965ca65fcb03dae853def05dc93789e114497
luis.iglesias <luisjavi27@gmail.com>
2022-08-08 16:39:42 -0500

    add pagination to API endpoints

--- src/controllers/API/productAPIControllers.js
+++ src/controllers/API/productAPIControllers.js
@@ -1,92 +1,122 @@
 const db = require("../../data/models");
 
 let productsController = {
-    getProducts: async (req, res) => {
-        try {
-            let products = await db.Product.findAndCountAll({
+  getProducts: async (req, res) => {
+    try {
+      const { page, size } = req.query;
 
-                attributes: [
-                    ["idProduct", "id"],
-                    ["title", "name"],
-                    "description"
-                ],
-                include: [
-                    {
-                        model: db.Category, as: "productCategory",
-                        through: {
-                            attributes: []
-                        }
-                    },
-                    
-                ],
-            });
+      const getPagination = (page, size) => {
+        const limit = size ? +size : 10;
+        const offset = page ? page * limit : 0;
+        return { limit, offset };
+      };
 
-            if (products === null) {
-                res.status(204).send()
-            } else {
-                products = JSON.parse(JSON.stringify(products, null, 2))
+      const getPagingData = (data, page, limit) => {
+        const { count, rows } = data;
+        const currentPage = page ? +page : 1;
+        const totalPages = Math.ceil(count / limit);
+        return { count, rows, totalPages, currentPage };
+      };
 
-                for (let i = 0; i < products.rows.length; i++) {
-                    products.rows[i].detail = [process.env.URL_Server || process.env.URL_DEV] + `${process.env.PORT}/api/products/${products.rows[i].id}`
-                }
-                res.status(200).send({ data: { count: products.count, products: products.rows } });
-            }
-        } catch (err) {
-            res.status(500).send({ error: err.toString() });
+      const { limit, offset } = getPagination(page, size);
+
+      let products = await db.Product.findAndCountAll({
+        limit,
+        offset,
+        attributes: [["idProduct", "id"], ["title", "name"], "description"],
+        include: [
+          {
+            model: db.Category,
+            as: "productCategory",
+            through: {
+              attributes: [],
+            },
+          },
+        ],
+        distinct: "idProduct",
+        order: [["idProduct", "ASC"]],
+      });
+
+      if (products === null) {
+        res.status(204).send();
+      } else {
+        products = getPagingData(products, page, limit);
+        products = JSON.parse(JSON.stringify(products, null, 2));
+        
+        for (let i = 0; i < products.rows.length; i++) {
+          products.rows[i].detail =
+            [process.env.URL_Server || process.env.URL_DEV] +
+            `${process.env.PORT}/api/products/${products.rows[i].id}`;
         }
-    },
-    getProduct: async (req, res) => {
-        try {
-            let product = await db.Product.findOne({
-                where: {
-                    idProduct: [req.params.id || ""],
-                },
-                attributes: [
-                    ["idProduct", "id"],
-                    ["title", "name"],
-                    "description",
-                    "stock",
-                    "price"
+        res
+          .status(200)
+          .send({
+            data: {
+              count: products.count,
+              products: products.rows,
+              totalPages: products.totalPages,
+              currentPage: products.currentPage,
+            },
+          });
+      }
+    } catch (err) {
+      res.status(500).send({ error: err.toString() });
+    }
+  },
+  getProduct: async (req, res) => {
+    try {
+      let product = await db.Product.findOne({
+        where: {
+          idProduct: [req.params.id || ""],
+        },
+        attributes: [
+          ["idProduct", "id"],
+          ["title", "name"],
+          "description",
+          "stock",
+          "price",
+        ],
+        include: [
+          { association: "productsImages", attributes: ["imageProduct"] },
+          {
+            association: "product_Color",
+            through: {
+              attributes: [],
+            },
+          },
+          {
+            association: "Product_Size",
+            through: {
+              attributes: [],
+            },
+          },
+          {
+            association: "productCategory",
+            through: {
+              attributes: [],
+            },
+          },
+        ],
+      });
 
-                ],
-                include: [
-                    { association: "productsImages",
-                    attributes:[
-                        
-                       "imageProduct"
-                        
-                    ] },
-                    { association: "product_Color",
-                    through: {
-                        attributes: []
-                    } },
-                    { association: "Product_Size",
-                    through: {
-                        attributes: []
-                    } },
-                    { association: "productCategory",
-                    through: {
-                        attributes: []
-                    } },
-                  ],
-            });
-
-            if (product === null) {
-                res.status(204).send()
-            } else {
-                product = JSON.parse(JSON.stringify(product, null, 2));
-                product.images =[];
-                for(let i=0; i< product.productsImages.length; i++){
-                    product.images.push([process.env.URL_Server || process.env.URL_DEV] + `${process.env.PORT}/products/image/${product.productsImages[i].imageProduct}`)
-                }
-                delete product.productsImages;
-                res.status(200).send({ data: product });
-            }
-        } catch (err) {
-            res.status(500).send({ error: err.toString() });
+      if (product === null) {
+        res.status(204).send();
+      } else {
+        product = JSON.parse(JSON.stringify(product, null, 2));
+        product.images = [];
+        for (let i = 0; i < product.productsImages.length; i++) {
+          product.images.push(
+            [process.env.URL_Server || process.env.URL_DEV] +
+              `${process.env.PORT}/products/image/${product.productsImages[i].imageProduct}`
+          );
         }
-    },
-
+        delete product.productsImages;
+        res.status(200).send({ data: product });
+      }
+    } catch (err) {
+      res.status(500).send({ error: err.toString() });
+    }
+  },
 };
 
 module.exports = productsController;
--- src/controllers/API/userAPIControllers.js
+++ src/controllers/API/userAPIControllers.js
@@ -1,59 +1,85 @@
 const db = require("../../data/models");
-const sequelize = require("Sequelize");
 
 let usersController = {
-    getUsers: async (req, res) => {
-        try {
-            let users = await db.User.findAndCountAll({
+  getUsers: async (req, res) => {
+    try {
+      const { page, size } = req.query;
+      const getPagination = (page, size) => {
+        const limit = size ? +size : 5;
+        const offset = page ? page * limit : 0;
+        return { limit, offset };
+      };
 
-                attributes: [
-                    ["idUser", "id"],
-                    ["fullname", "name"],
-                    "email"],
-                raw: true,
-            });
+      const { limit, offset } = getPagination(page, size);
+      const getPagingData = (data, page, limit) => {
+        const { count, rows } = data;
+        const currentPage = page ? +page : 1;
+        const totalPages = Math.ceil(count / limit);
+        return { count, rows, totalPages, currentPage };
+      };
 
-            if (users === null) {
-                res.status(204).send()
-            } else {
-                for (let i = 0; i < users.rows.length; i++) {
-                    users.rows[i].detail = [process.env.URL_Server || process.env.URL_DEV] + `${process.env.PORT}/api/users/${users.rows[i].id}`
-                }
-                res.status(200).send({ data: {count: users.count, users: users.rows} });
-            }
-        } catch (err) {
-            res.status(500).send({ error: err.toString() });
+      let users = await db.User.findAndCountAll({
+        limit,
+        offset,
+        attributes: [["idUser", "id"], ["fullname", "name"], "email"],
+        distinct: "idUser",
+        order: [["idUser", "ASC"]],
+      });
+
+      if (users === null) {
+        res.status(204).send();
+      } else {
+        users = getPagingData(users, page, limit);
+        users = JSON.parse(JSON.stringify(users, null, 2));
+
+        for (let i = 0; i < users.rows.length; i++) {
+          users.rows[i].detail =
+            [process.env.URL_Server || process.env.URL_DEV] +
+            `${process.env.PORT}/api/users/${users.rows[i].id}`;
         }
-    },
-    getUser: async (req, res) => {
-        try {
-            let user = await db.User.findOne({
-                where: {
-                    idUser: [req.params.id || ""],
-                },
-                attributes: [
-                    ["idUser", "id"],
-                    ["fullname", "name"],
-                    "email",
-                    "addres",
-                    "birthday",
-                    "user",
-                    "userimage"
-                ],
-                raw: true,
-            });
+        res.status(200).send({
+          data: {
+            count: users.count,
+            users: users.rows,
+            totalPages: users.totalPages,
+            currentPage: users.currentPage,
+          },
+        });
+      }
+    } catch (err) {
+      res.status(500).send({ error: err.toString() });
+    }
+  },
+  getUser: async (req, res) => {
+    try {
+      let user = await db.User.findOne({
+        where: {
+          idUser: [req.params.id || ""],
+        },
+        attributes: [
+          ["idUser", "id"],
+          ["fullname", "name"],
+          "email",
+          "addres",
+          "birthday",
+          "user",
+          "userimage",
+        ],
+        raw: true,
+      });
 
-            if (user === null) {
-                res.status(204).send()
-            } else {
-                user.image_url = [process.env.URL_Server || process.env.URL_DEV] + `${process.env.PORT}/users/image/${user.userimage}`
-                res.status(200).send({ data: user });
-            }
-        } catch (err) {
-            res.status(500).send({ error: err.toString() });
-        }
-    },
-
+      if (user === null) {
+        res.status(204).send();
+      } else {
+        user.image_url =
+          [process.env.URL_Server || process.env.URL_DEV] +
+          `${process.env.PORT}/users/image/${user.userimage}`;
+        res.status(200).send({ data: user });
+      }
+    } catch (err) {
+      res.status(500).send({ error: err.toString() });
+    }
+  },
 };
 
 module.exports = usersController;
