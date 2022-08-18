c9a6dbaafa34a59fc3a87d1c99da1962d2801df5
luis.iglesias <luisjavi27@gmail.com>
2022-08-18 18:16:20 -0500

    minor-changes in views

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,24 +1,34 @@
-
 const slidesContainer = document.getElementById("mySlides");
 const slide = document.querySelector(".bannerImagen");
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
- 
+
 nextButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
- setTimeout(() => nextButton.click(), 3000);
+  if (slidesContainer.scrollLeft >= (slideWidth * 4)) {
+    slidesContainer.scrollTo({
+      top:0, 
+      left: -(slideWidth * 5),
+      behavior:"instant"
+    })
+  }else{
+
+    slidesContainer.scrollLeft += slideWidth;
+  }
+  setTimeout(() => nextButton.click(), 3000);
 
 });
- 
+
 prevButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth*5;
- setTimeout(() => prevButton.click(), 15000);
-
+  // slidesContainer.scrollLeft -= slideWidth * 5;
+  slidesContainer.scrollTo({
+    top:0, 
+    left: -(slideWidth * 5),
+    behavior:"instant"
+  })
+  console.log("-", slideWidth * 5);
+  setTimeout(() => prevButton.click(), 15000);
 });
 
- setTimeout(() => nextButton.click(), 3000);
- setTimeout(() => prevButton.click(), 15000);
-
-
+setTimeout(() => nextButton.click(), 3000);
